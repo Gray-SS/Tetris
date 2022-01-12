aebaa55eb943f1762b4b2c9e58e92b84a52503a1
Espace6I_2021 <Espace6I_2021@APMW969>
2022-01-12 11:47:23 +0100

    tkt

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Tetris
 {
@@ -13,15 +14,25 @@ namespace Tetris
 
     public class TetrisEngine : Engine
     {
+        private Tile[,] _tetris_sandbox;
         private Rectangle _tetrisViewport;
 
         public TetrisEngine() : base() { }
 
         public override void Load()
         {
-            _tetrisViewport = new Rectangle(2, 1, ConsoleWidth / 4, ConsoleHeight - 2);
-        }
+            _tetrisViewport = new Rectangle(2, 1, 19, ConsoleHeight - 2);
+            _tetris_sandbox = new Tile[_tetrisViewport.Width, _tetrisViewport.Height];
 
+            for (int i = 0; i < _tetris_sandbox.GetLength(1); i++)
+            {
+                for (int j = 0; j < _tetris_sandbox.GetLength(0); j++)
+                {
+                    _tetris_sandbox[j, i] = new Tile() { X = j + _tetrisViewport.X, Y = i + _tetrisViewport.Y, Value = new BufferValue(null, null, CColor.Gray) };
+                }
+            }
+        }
+        
         public override void Update(float dt)
         {
             
@@ -36,9 +47,26 @@ namespace Tetris
             Graphics.DrawRectangle(1, ConsoleHeight - 1, 20, 1, null, null, CColor.DarkGray);
             Graphics.DrawRectangle(21, 0, 2, ConsoleHeight, null, null, CColor.DarkGray);
 
+            foreach (var tile in _tetris_sandbox)
+            {
+                tile.Draw(Graphics);
+            }
+
             Graphics.DrawText(ConsoleWidth - 20, 1, $"FPS: {FPS}", CColor.Red, null);
             Graphics.DrawText(ConsoleWidth - 20, 2, $"Console Width: {ConsoleWidth}", CColor.Green, null);
             Graphics.DrawText(ConsoleWidth - 20, 3, $"Console Height: {ConsoleHeight}", CColor.Blue, null);
+        }
+    }
+
+    public class Tile
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public BufferValue Value { get; set; }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawRectangle(X, Y, 1, 1, Value.Char, Value.Color, Value.BackgroundColor);
         }
     }
 }
